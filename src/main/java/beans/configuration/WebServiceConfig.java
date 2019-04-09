072d6cb350a43bcd7b384c01839d09a98c382e93
Stanislav <stasn@ua.fn>
2019-04-10 01:18:07 +0300

    change config change servlet

--- src/main/java/beans/configuration/WebServiceConfig.java
+++ src/main/java/beans/configuration/WebServiceConfig.java
@@ -1,13 +1,13 @@
 package beans.configuration;
 
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.ApplicationContext;
+//import org.springframework.boot.web.servlet.ServletRegistrationBean;
+//import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
-import org.springframework.ws.transport.http.MessageDispatcherServlet;
+//import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
@@ -15,19 +15,19 @@ import org.springframework.xml.xsd.XsdSchema;
 @EnableWs
 @Configuration
 public class WebServiceConfig extends WsConfigurerAdapter {
-    @Bean
-    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext) {
-        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
-        servlet.setApplicationContext(applicationContext);
-        servlet.setTransformWsdlLocations(true);
-        return new ServletRegistrationBean<>(servlet, "/ws/*");
-    }
+//    @Bean
+//    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext) {
+//        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+//        servlet.setApplicationContext(applicationContext);
+//        servlet.setTransformWsdlLocations(true);
+//        return new ServletRegistrationBean<>(servlet, "/ws/*");
+//    }
 
-    @Bean(name = "event_user")
+    @Bean(name = "event")
     public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema usersEventsSchema) {
         DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
         wsdl11Definition.setPortTypeName("UserPort");
-        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setLocationUri("//soapws");
         wsdl11Definition.setTargetNamespace("http://beans.models.xml/user-web.service");
         wsdl11Definition.setSchema(usersEventsSchema);
         return wsdl11Definition;
